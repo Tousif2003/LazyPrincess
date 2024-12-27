47fc305d1f8e44dd9335294e704219809052f6ad
Tousif2003 <135956946+Tousif2003@users.noreply.github.com>
2024-12-27 12:52:51 +0530

    Create exceptions.py

---
+++ server/exceptions.py
@@ -0,0 +1,9 @@
+    # 🥰  Thank you for helping  this journey  @LazyDeveloperr  🥰
+    # thanks Adarsh Goel for Server Routing
+    #thanks @Filmymod7
+
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
